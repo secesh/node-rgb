73cd3c443b312d2fa632e27e3a3f44273fb733eb
Matthew R Chase <matt@chasefox.net>
2013-07-23 19:14:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+rgbLED
+======
+
+This project provides a communication driver, written for Node.JS, to ease the communication with
+The WiFi-CON (aka MiLight aka EasyBulb), a device that provides network access to an RGB LED 
+controller.  The WiFi-CON can act as a wifi client, which enables it to integrate with your existing
+LAN.  It can also act as a WiFi Access Point to provide a stand-alone network (enabling client access
+from your smartphone).  The initial commit of this project does not include information about how to
+configure your WiFi-CON.  It features a rather unintuitive, buggy, and poorly documented management
+portal.
+
+With the WiFi-CON integrated to a LAN, this package provides programmatic access allowing you
+to easily communicate with your color-changeable, dimming LED lights and light strips!
+
+## Install
+
+```bash
+npm install rgbLED
+```
+
+## Basic Usage
+In this example, we will connect to the controller, turn the lights on, and set the color.
+
+```node
+var lights = require("rgbLED")
+
+lights.setHost("led") //This argument can be a hostname or IP Address.
+lights.turnOn()
+
+lights.setColor(189) //Color is a one-byte value.  In decimal, this is expressed in the range 0-255.
+```
+
+## Intermediate usage
+In this example, we'll build on the basic usage example by setting the lights to slowly change.
+We provide a simple routine that will increment the color of the lights one step every 112 seconds.
+Because the LED controller supports 256 colors, this cycle will take about 8 hours to complete.
+And then it starts over and just keeps going.
+
+```node
+var lights = require("rgbLED")
+lights.setHost("led")
+lights.turnOn()
+var color = 0
+
+lights.setColor(color)
+setInterval(function(){
+    lights.setColor(color)
+    color++
+    if(color >= 256) color = 0
+}, 112000)
+```
+
+## The hardware
+Apparently the WiFi module works with multiple products.  It's super cheap generic hardware 
+manufactured in china.  It's not obvious what all it works with or who really makes it, though
+it bears a copyright from CEC Huada Electronic Design Co., Ltd.  Are they DBA HED International?
+Because that's who produced the android application from which the protocol was discovered.
+
+Anyway, we came across the device as a way to bridge LED light strips to a network, so that's 
+what we'll give as an example.  In this case, you need two controllers.  One connects to your 
+LED strips, the other is the WiFi module.  Both are supplied by superbrightleds.com.  This project
+is not associated with that site; we simply love the fact that they sell the wifi module, which
+is awesome, and we wanted to enable programmatic access to it.  Doing so took just a bit of
+reverse-engineering the protocol.
+Here's the controller you need: [goo.gl/WCqUL](http://goo.gl/WCqUL)
+and here's the wifi module: [goo.gl/uOjA9F](http://goo.gl/uOjA9F)
+
+## Thanks
+We know this isn't a large project.  But we thought it was awesome fun and hope you totally
+enjoy building upon this.  Tell us all about it if you like; we love to be inspired.  If 
+you're feeling generous, the tip jar can be found here: <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="55A8WG9PDX2AU">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
---
+++ index.js
@@ -0,0 +1,64 @@
+/********************************************************************************
+Copyright 2013 Matthew R Chase (matt@chasefox.net)
+This file is part of node-rgb (http://github.com/secesh/node-rgb/).
+
+    node-rgb is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    node-rgb is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with node-rgb.  If not, see <http://www.gnu.org/licenses/>.
+********************************************************************************/
+
+var dgram = require('dgram')
+
+var sendMessage = function(command, value, callback, id){
+	/*****************************
+	This function sends a message to the superbrightleds.com WiFi-CON.  Reverse-engineering of the protocol
+	discovered that it responds to three byte UDP messages.  The first byte is the command to perform.  The
+	second byte is the value to send with the command.  The third byte is always the same, and therefore
+	appears to act as an endOfTransmission flag.
+
+		Arguments:
+	    	command  : a decimal value.
+	    	value    : a decimal value to be sent with the command.
+	    	id       : optional value used to identify the message.  Can be used for
+	    	           tracking the message through the asynchronous transmission process.
+	    	callback : optional function will be called when sending is complete.  Callback will be called
+	    	           with two arguments.  The first being any error returned by the datagram.send function,
+	    	           the second being the optional id value.
+	************************************************************************************************************/
+ 	if(!HOST) return callback("Host address of WiFi-CON not set.  Before sending commands, you must call the setHost function.")
+ 	var etx = 85 //hex value 55
+	
+	var m = new Buffer([command, value, etx])
+	var socket = dgram.createSocket("udp4")
+	socket.send(m, 0, m.length, PORT, HOST, function(e,b){
+		//http://nodejs.org/api/dgram.html#dgram_socket_send_buf_offset_length_port_address_callback
+		//It appears the second argument is the number of bytes sent.  I'm not sure what purpose it
+		//serves, though.
+		socket.close()
+		if(callback) callback(e, id)
+	})
+}
+
+var PORT = 50000, HOST = "192.168.1.100"
+//when calling setHost, the argument should be a string.  The value can be either an IP address or a host name.
+exports.setHost   = function(host){ HOST = host }
+
+exports.setColor  = function(color, callback, id){ sendMessage(32,color,callback,id) }
+
+exports.turnOff   = function(callback, id){ sendMessage(33,00,callback,id) }
+exports.turnOn    = function(callback, id){ sendMessage(34,00,callback,id) }
+exports.brighten  = function(callback, id){ sendMessage(35,00,callback,id) }
+exports.dim       = function(callback, id){ sendMessage(36,00,callback,id) }
+exports.speedUp   = function(callback, id){ sendMessage(37,00,callback,id) }
+exports.speedDown = function(callback, id){ sendMessage(38,00,callback,id) }
+exports.modeUp    = function(callback, id){ sendMessage(39,00,callback,id) }
+exports.modeDown  = function(callback, id){ sendMessage(40,00,callback,id) }
